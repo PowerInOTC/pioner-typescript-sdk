305ae71119dd013e459bc84395ae992ece34faef
Nebule <horseheadb33@proton.me>
2024-03-18 17:31:46 +0100

    fixed types export

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,6 +1,4 @@
 export * from './apiModule';
 export * from './rfqWebsocket';
 export * from './quoteWebsocket';
-export * from './types/price';
-export * from './types/requests';
-export * from './types/responses';
+export * from './types';
---
+++ packages/api-client/src/types/index.ts
@@ -0,0 +1,3 @@
+export * from './price';
+export * from './requests';
+export * from './responses';
